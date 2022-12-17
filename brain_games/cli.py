4f2b2301a72a4c41944e0e01198011740bee1d81
kuznetsovyar22 <kuznetsovyar@mail.ru>
2022-12-17 16:46:04 +0300

    4th

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,11 +1,14 @@
 import prompt
 
+
 def welcome_user():
     print('Welcome to the Brain Games!')
-    name = prompt.string('May I have your name? ')
+    prompt.string('May I have your name? ')
+
 
 def main():
     welcome_user()
 
-if __name__ == '__main__':   
+
+if __name__ == '__main__':
     main()
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
-from ..cli import *
+from brain_games.cli import welcome_user
+
 
 def greet():
     print('Welcome to the Brain Games!')
 
+
 def main():
     welcome_user()
 
-if __name__ == '__main__':   
+
+if __name__ == '__main__':
     main()
