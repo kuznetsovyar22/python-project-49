262ebb4d0efe9a6722b5fa4cb8a87d1a3dfb09db
kuznetsovyar22 <kuznetsovyar@mail.ru>
2022-12-17 13:25:49 +0300

    third

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,8 +1,8 @@
-#!/usr/bin/env python3
-from ..cli import *
+import prompt
 
-def greet():
+def welcome_user():
     print('Welcome to the Brain Games!')
+    name = prompt.string('May I have your name? ')
 
 def main():
     welcome_user()
